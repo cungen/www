a016e6c4b1ebf8147b0503652d49f31b03c45822
cungen <1360571582@qq.com>
2015-12-25 18:01:23 +0800

    add aside component and task list component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/lib/app-bar';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import IconButton from 'material-ui/lib/icon-button';
+import MenuItem from 'material-ui/lib/menus/menu-item';
 
+import MoreVertIcon from 'material-ui/lib/svg-icons/action/swap-vert'
 export default class App extends Component {
     render() {
+        const styles = {
+            content: {
+                position: 'absolute',
+                top: 64,
+                right: 0,
+                bottom: 0,
+                left: 0
+            }
+        };
         return (
-            <h1>Hello, world.</h1>
+            <div>
+                <AppBar
+                    title="App"
+                    iconElementRight={
+                        <IconMenu iconButtonElement={
+                          <IconButton><MoreVertIcon /></IconButton>
+                        }>
+                          <MenuItem primaryText="Refresh" />
+                          <MenuItem primaryText="Help" />
+                          <MenuItem primaryText="Sign out" />
+                        </IconMenu>
+                    } />
+                <div style={styles.content}>
+                    {this.props.children}
+                </div>
+            </div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import {Router, Route, Link } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import App from './App';
+import App from './app';
+import About from './about';
+import Aside from './aside';
+import TaskList from './tasks/task-list';
 
 injectTapEventPlugin();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render((
+    <Router>
+        <Route path='/' component={App} >
+            <Route path="Todo" component={About} />
+            <Route path="aside" component={Aside} />
+            <Route path="login" component={About} />
+            <Route path="tasks" component={TaskList} />
+        </Route>
+    </Router>
+), document.getElementById('root'));
