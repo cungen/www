2599dcf087968c87f842f8785ae4ac6d001eff48
Cungen <1360571582@qq.com>
2016-06-27 23:59:21 +0800

    update npm config

--- src/components/partials/header.js
+++ src/components/partials/header.js
@@ -3,9 +3,6 @@ import { assign } from 'lodash';
 
 export default class Header extends Component {
     render() {
-        const styles = {
-        };
-
         return (
             <div>
                 <div>Cungen</div>
@@ -13,13 +10,6 @@ export default class Header extends Component {
                     <li>About</li>
                     <li>Articles</li>
                     <li>
-                        <Popover open={this.state.activePopover === 'pop'}
-                                 anchorEl={this.state.anchorEl}
-                                 anchorOrigin={anchorOrigin}
-                                 targetOrigin={targetOrigin}
-                                 onRequestClose={this.closePopover.bind(this, 'pop')} >
-                            <AppList />
-                        </Popover>
                     </li>
                 </ul>
             </div>
